10c633ef39e671d1224cbc4c3917facc2dc1c453
xjxliuxing@163.com <19900713op>
2021-09-16 17:29:11 +0800

    优化banner

--- app/src/main/java/com/android/app/ui/activity/animation/ViewPagerActivity.kt
+++ app/src/main/java/com/android/app/ui/activity/animation/ViewPagerActivity.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import com.android.app.R
 import com.android.app.databinding.ActivityViewPagerBinding
 import com.android.helper.base.BaseBindingActivity
+import com.android.helper.utils.ToastUtil
+import com.android.helper.widget.banner.BannerItemClickListener
 import com.android.helper.widget.banner.BannerLoadListener
 
 /**
@@ -17,39 +19,35 @@ class ViewPagerActivity : BaseBindingActivity<ActivityViewPagerBinding>() {
     override fun initData() {
         setTitleContent("自定义ViewPager的类")
 
-//        val list: ArrayList<Int> = ArrayList()
-//        list.add(R.mipmap.icon_banner_1)
-//        list.add(R.mipmap.icon_banner_2)
-//        list.add(R.mipmap.icon_banner_3)
-//        list.add(R.mipmap.icon_banner_4)
+        val list: ArrayList<Int> = ArrayList()
+        list.add(R.mipmap.icon_banner_1)
+        list.add(R.mipmap.icon_banner_2)
+        list.add(R.mipmap.icon_banner_3)
+        list.add(R.mipmap.icon_banner_4)
 
         val listView = arrayListOf<View>()
         listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner, null))
         listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner2, null))
         listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner3, null))
-        listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner4, null))
-        listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner5, null))
+        listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner, null))
+        listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner2, null))
+        listView.add(LayoutInflater.from(mContext).inflate(R.layout.fragment_vp_banner3, null))
 
-        mBinding.bannerView.setImageData(listView)
-            .setBannerLoadListener(BannerLoadListener<Int> { view, _, t ->
-                if (view is ImageView) {
-                    view.setImageResource(t!!)
+
+        mBinding.bannerView
+            .setImageData(listView)
+            .setImageData(list)
+            .setBannerLoadListener(object : BannerLoadListener<Int> {
+                override fun onLoadView(view: ImageView?, position: Int, t: Int?) {
+                    view?.setImageResource(t!!)
                 }
             })
-            .addIndicator(mBinding.biBanner)
+            .setItemClickListener(object : BannerItemClickListener<Int> {
+                override fun onItemClick(view: View?, position: Int, t: Int?) {
+                    ToastUtil.show("Position:" + position)
+                }
+            })
             .show(this)
-
-//        val fragmentList = ArrayList<Fragment>()
-//        fragmentList.add(VpBanner1Fragment.newInstance())
-//        fragmentList.add(VpBanner2Fragment.newInstance())
-//        fragmentList.add(VpBanner3Fragment.newInstance())
-//        fragmentList.add(VpBanner4Fragment.newInstance())
-//        fragmentList.add(VpBanner5Fragment.newInstance())
-//
-//        mBinding.bannerFragment
-//            .setFragmentData(fragmentList)
-//            .addIndicator(mBinding.biBanner2)
-//            .show(mContext)
     }
 
     override fun getBinding(
