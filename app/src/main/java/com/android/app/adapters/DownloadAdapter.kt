6f3d8e725b0a2f9efde7d44945bca9a5cde2603e
xjxliuxing@163.com <19900713op>
2021-11-18 11:29:39 +0800

    下载进度的更换3

--- app/src/main/java/com/android/app/adapters/DownloadAdapter.kt
+++ app/src/main/java/com/android/app/adapters/DownloadAdapter.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
     BaseRecycleAdapter<Download, DownloadAdapter.DlHolder>(mContext, mList) {
-
+    
     private val downLoadManager: DownLoadManager by lazy {
         return@lazy DownLoadManager
             .Builder()
@@ -25,21 +25,21 @@ class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
             .bindDownload(mContext)
             .build()
     }
-
+    
     class DlHolder(itemView: View) : BaseVH(itemView) {
-
+        
         val tv_download: Button = itemView.findViewById(R.id.tv_download)
         val tv_cancel: Button = itemView.findViewById(R.id.tv_cancel)
         val tv_current_progress: TextView = itemView.findViewById(R.id.tv_current_progress)
         val progress: ProgressBar = itemView.findViewById(R.id.progress)
     }
-
+    
     override fun onBindHolder(holder: DlHolder, position: Int) {
         val bean = mList[position]
         val tempFileLength = bean.tempFileLength
         val contentLength = bean.contentLength
         val idValue = bean.id
-
+        
         if ((tempFileLength > 0) && (tempFileLength < contentLength)) {
             holder.tv_download.text = "继续下载"
             val fl = tempFileLength.toFloat() / contentLength * 100
@@ -48,7 +48,7 @@ class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
             // 百分比进度
             holder.tv_current_progress.text = fl.toString()
         }
-
+        
         holder.tv_download.setOnClickListener {
             downLoadManager.download(bean, object : ProgressListener {
                 override fun onStart(id: String?, contentLength: Long) {
@@ -56,23 +56,24 @@ class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
                         holder.tv_download.text = "下载中"
                     }
                 }
-
-                override fun onProgress(id: String?, progress: Double, contentLength: Long, percentage: String?) {
+                
+                override fun onProgress(id: String, progress: Long, contentLength: Long, percentage: String) {
                     if (TextUtils.equals(id, idValue)) {
                         bean.contentLength = contentLength
-                        bean.tempFileLength = progress.toLong()
-
-                        holder.progress.progress = (progress / contentLength * 100).toInt()
+                        bean.tempFileLength = progress
+                        
+                        holder.progress.progress = progress.toInt()
+                        holder.progress.max = contentLength.toInt()
                         holder.tv_current_progress.text = percentage
                     }
                 }
-
+                
                 override fun onError(id: String?, throwable: Throwable?) {
                     if (TextUtils.equals(id, idValue)) {
                         holder.tv_download.text = "开始下载"
                     }
                 }
-
+                
                 override fun onComplete(id: String?, path: String?, response: Response?) {
                     if (TextUtils.equals(id, idValue)) {
                         holder.tv_download.text = "下载完成"
@@ -83,16 +84,16 @@ class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
                 mItemClickListener.onItemClick(holder.tv_download, position, bean)
             }
         }
-
+        
         holder.tv_cancel.setOnClickListener {
             // 取消下载
             downLoadManager.cancel(bean.id)
-
+            
             if (mItemClickListener != null) {
                 mItemClickListener.onItemClick(holder.tv_cancel, position, bean)
             }
         }
-
+        
         holder.itemView.setOnClickListener {
             if (mItemClickListener != null) {
                 val bindingAdapterPosition = holder.bindingAdapterPosition
@@ -104,14 +105,14 @@ class DownloadAdapter(mContext: FragmentActivity, mList: ArrayList<Download>) :
             }
         }
     }
-
+    
     /**
      * @return 返回一个RecycleView的布局
      */
     override fun getLayout(viewType: Int): Int {
         return R.layout.item_download
     }
-
+    
     override fun createViewHolder(inflate: View?, viewType: Int): DlHolder {
         return DlHolder(inflate!!)
     }
