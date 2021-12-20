b64a716473781dc0fdcecba1316e0951573d0a5f
xjxliuxing@163.com <xjxliuxing@163.com>
2021-12-21 00:03:18 +0800

    钉钉的账号保活

--- app/src/main/java/com/android/app/ui/activity/personal/RouseDingDingActivity.kt
+++ app/src/main/java/com/android/app/ui/activity/personal/RouseDingDingActivity.kt
@@ -21,11 +21,11 @@ import com.amap.api.services.core.LatLonPoint
 import com.amap.api.services.route.DistanceSearch
 import com.amap.api.services.route.DistanceSearch.DistanceQuery
 import com.android.app.R
-import com.android.app.app.Keepalive.keep.KeepManager
+import com.android.app.app.Keepalive.LifecycleManager
 import com.android.app.databinding.ActivityRouseDingDingBinding
+import com.android.helper.app.BaseApplication
 import com.android.helper.base.title.BaseBindingTitleActivity
 import com.android.helper.utils.*
-import com.android.helper.utils.account.AccountHelper
 import com.android.helper.utils.location.LocationUtil
 
 /**
@@ -38,7 +38,7 @@ import com.android.helper.utils.location.LocationUtil
  *      5：唤醒钉钉
  */
 class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBinding>() {
-    
+
     private lateinit var myLocationStyle: MyLocationStyle
     private lateinit var mAMap: AMap
     private var mCityCode = "" // 城市编码
@@ -54,7 +54,7 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
                         val latLng = LatLng(latLonPoint.latitude, latLonPoint.longitude)
                         SpUtil.putString("latitude", latLonPoint.latitude.toString())
                         SpUtil.putString("longitude", latLonPoint.longitude.toString())
-                        
+
                         addMarker(latLng, title!!, "")
                         moveMap(latLng)
                     }
@@ -65,18 +65,18 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
             }
         }
     }
-    
+
     override fun setTitleContent(): String {
         return "唤醒钉钉"
     }
-    
+
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivityRouseDingDingBinding {
         return ActivityRouseDingDingBinding.inflate(inflater, container, true)
     }
-    
+
     override fun initData(savedInstanceState: Bundle?) {
         lifecycle()
-        
+
         notification()
         // //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mBinding.mapView.onCreate(savedInstanceState)
@@ -93,7 +93,7 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
                     val distance = distanceItem.distance
                     // 行驶时间 单位：秒
                     val duration = distanceItem.duration
-                    
+
                     ToastUtil.show("距离：$distance   时间：$duration")
                     LogUtil.e("距离：$distance   时间：$duration")
                 }
@@ -133,16 +133,16 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
                         // 6:测量距离请求接口，调用后会发起距离测量请求。
                         distanceSearch.calculateRouteDistanceAsyn(distanceQuery)
                     }
-                    
+
                     if (!mMoved) {
                         LogUtil.e("latitude:$latitude  longitude:$longitude")
                         val latLng = LatLng(latitude, longitude)
                         moveMap(latLng)
                         // 定位蓝点
                         locationPoint()
-                        
+
                         touchSetting()
-                        
+
                         mMoved = true
                     }
                 }
@@ -167,7 +167,7 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
             }
         }
     }
-    
+
     private fun notification() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
             val serviceRunning = ServiceUtil.isServiceRunning(mContext, NotificationService::class.java)
@@ -185,12 +185,12 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
             }
         }
     }
-    
+
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         LogUtil.e("onNewIntent:")
     }
-    
+
     private fun addMarker(latLng: LatLng, title: String, description: String) {
         // 添加标记点
         val addMarker = mAMap.addMarker(MarkerOptions().position(latLng))
@@ -239,51 +239,45 @@ class RouseDingDingActivity : BaseBindingTitleActivity<ActivityRouseDingDingBind
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
         // 控制是否显示定位蓝点
         myLocationStyle.showMyLocation(true)
-        
+
         myLocationStyle.strokeColor(ResourceUtil.getColor(R.color.red_9))//设置定位蓝点精度圆圈的边框颜色的方法。
         // myLocationStyle.radiusFillColor(ResourceUtil.getColor(R.color.green_3))//设置定位蓝点精度圆圈的边框颜色的方法。
         myLocationStyle.strokeWidth(10f) // 设置定位蓝点精度圈的边框宽度的方法。
         mAMap.myLocationStyle = myLocationStyle //设置定位蓝点的Style
         mAMap.isMyLocationEnabled = true;// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
     }
-    
+
+    /**
+     * 账号保活
+     */
     private fun lifecycle() {
-        // 1:账号保活
-        // 1:账号保活
-        val accountHelper = AccountHelper.getInstance()
-        accountHelper
-            .addAccountType(application.resources.getString(R.string.account_type))
-            .addAccountAuthority(application.resources.getString(R.string.account_authority))
-            .addAccountName(application.resources.getString(R.string.account_name))
-            .addAccountPassword(application.resources.getString(R.string.account_password))
-            .addAccount(application) //添加账户
-        accountHelper.autoSync()
-        // 2:后台服务
-        val intent = Intent(this, MapService::class.java)
-        ServiceUtil.startService(application, intent)
-        // 4:屏幕一像素保活，适用于8.0以下的手机
-        KeepManager.getInstance().registerKeep(application)
+        val serviceName = "com.android.app.ui.activity.personal.MapService";
+        val jobServiceName = "com.android.app.app.Keepalive.AppJobService";
+
+        LifecycleManager
+            .getInstance() // com.android.app.ui.activity.personal.MapService
+            .startLifecycle(BaseApplication.getApplication(), serviceName, jobServiceName)
     }
-    
+
     //</editor-fold>
     override fun onResume() {
         super.onResume()
         //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
         mBinding.mapView.onResume()
     }
-    
+
     override fun onPause() {
         super.onPause()
         //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
         mBinding.mapView.onPause()
     }
-    
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mBinding.mapView.onSaveInstanceState(outState)
     }
-    
+
     override fun onDestroy() {
         super.onDestroy()
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
