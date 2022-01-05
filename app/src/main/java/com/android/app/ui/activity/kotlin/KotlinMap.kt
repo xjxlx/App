3532fea0576188f14bc90626c6f6f5314fad86c4
xjxliuxing@163.com <xjxliuxing@163.com>
2022-01-05 09:37:20 +0800

    111

--- app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
+++ app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.android.app.databinding.ActivityKotlinMapBinding
 import com.android.helper.base.title.BaseBindingTitleActivity
+import com.android.helper.utils.LogUtil
 
 /**
  * @author XJX
@@ -16,6 +17,38 @@ class KotlinMap : BaseBindingTitleActivity<ActivityKotlinMapBinding>() {
     var ssss: String = "sss"
 
     override fun initData(savedInstanceState: Bundle?) {
+        val s = "abcdefg"
+        var a = s[0]
+        LogUtil.e("abc  ${a + "sss"}")
+
+        LogUtil.e("{\"key+:\"value\"}")
+        LogUtil.e("{\"key:value}\"")
+        LogUtil.e("{$s:$a}")
+        // && 和 ||
+        val a1 = true
+        val a2 = false
+        val and = a1.and(a2)
+        val and2 = a2.or(a1)
+        if (and) {
+            LogUtil.e("true")
+        } else {
+            LogUtil.e("false")
+        }
+        if (and2) {
+            LogUtil.e("true")
+        } else {
+            LogUtil.e("false")
+        }
+        LogUtil.e("java && " + { and })
+        // 区间运算符
+        for (a in 1..3) {
+            LogUtil.e("a :$a")
+        }
+
+        for (b in 3 downTo 1) {
+            LogUtil.e("b :$b")
+        }
+
     }
 
     override fun setTitleContent(): String {
