2cec68bcd0e767c22795ff845b9c47db3b6af04b
xjxliuxing@163.com <xjxliuxing@163.com>
2022-01-08 22:09:24 +0800

    充电进度条

---
+++ app/src/main/java/com/android/app/ui/activity/widget/CustomTestViewActivity.java
@@ -0,0 +1,31 @@
+package com.android.app.ui.activity.widget;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.android.app.databinding.ActivityCustomTestBinding;
+import com.android.helper.base.title.BaseBindingTitleActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CustomTestViewActivity extends BaseBindingTitleActivity<ActivityCustomTestBinding> {
+
+    @Override
+    protected String setTitleContent() {
+        return "自定义view的练习";
+    }
+
+    @Override
+    public ActivityCustomTestBinding getBinding(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container) {
+        return ActivityCustomTestBinding.inflate(inflater, container, true);
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+
+    }
+}
