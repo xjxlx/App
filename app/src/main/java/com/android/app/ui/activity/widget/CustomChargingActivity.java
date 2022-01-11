001b4065523b45d51747b146d5abe72e881aa610
xujixing01@souche.com <19900713op>
2022-01-11 19:21:38 +0800

    进度条 ---> 完善

--- app/src/main/java/com/android/app/ui/activity/widget/CustomChargingActivity.java
+++ app/src/main/java/com/android/app/ui/activity/widget/CustomChargingActivity.java
@@ -3,12 +3,14 @@ package com.android.app.ui.activity.widget;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.android.app.databinding.ActivityCustomChargingBinding;
 import com.android.helper.base.title.BaseBindingTitleActivity;
+import com.android.helper.utils.LogUtil;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,6 +27,33 @@ public class CustomChargingActivity extends BaseBindingTitleActivity<ActivityCus
 
     @Override
     public void initData(Bundle savedInstanceState) {
+
+        mBinding.pb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                LogUtil.e("progress:" + progress);
+
+                float v = progress * 0.1f;
+                float v1 = v / 10;
+                LogUtil.e("v:" + v + "   v1:" + v1);
+
+                mBinding.cpv.setPercentage(v1);
+                mBinding.cpv.setInterval(0.3f, 0.9f);
+                mBinding.cpv.setOptimumValue(0.8f);
+                mBinding.cpv.setRemainingChargeTime("5小时20分");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
