f5945e91ed5bb30f4e4cce24cfa473b5d4267cdc
xjxliuxing@163.com <xjxliuxing@163.com>
2022-01-18 01:40:10 +0800

    保存设置

--- app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
+++ app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
@@ -19,37 +19,43 @@ class KotlinMap : AppBaseBindingTitleActivity<ActivityKotlinMapBinding>() {
     override fun initData(savedInstanceState: Bundle?) {
         // 指定类型
         var array1 = arrayOf(1, 2, 3)
+
         // 指定长度
         var array2 = BooleanArray(1)
+
         // 不指定类型的数组，自动推断，但是不能为null
         val array3 = arrayOf(5)
+
         // array3[2] = null
         // 可空的数组
         val arrayOfNulls = arrayOfNulls<String>(3)
         arrayOfNulls[1] = null
         arrayOfNulls[2] = ""
-
         val intArray = IntArray(4) { 21 }
-
         // 角标便利
         for (a in 0 until intArray.size) {
             LogUtil.e("a 1:" + intArray[a])
         }
-
         // 高级角标便利
         for (a in intArray.indices) {
             LogUtil.e("a 2:" + intArray[a])
 
         }
-
         // 元素便利
         for (element in intArray) {
             LogUtil.e("a 3:$element")
         }
-
         // 同时便利角标和元素
-        for ((index,item)in intArray.withIndex()){
+        for ((index, item) in intArray.withIndex()) {
             LogUtil.e("角标：$index 元素：$item")
+        }
+        // forEach 循环,it代表数据的元素
+        intArray.forEach {
+            LogUtil.e("forEach:$it")
+        }
+        // 增强版的forEach
+        intArray.forEachIndexed { index, item ->
+            LogUtil.e("增强forEach:  index：$index   item:$item")
         }
     }
 
