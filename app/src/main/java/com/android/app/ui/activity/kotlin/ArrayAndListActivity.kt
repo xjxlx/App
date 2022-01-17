8fc4371f46365a79c82150e91de8f59d80f7bbae
xjxliuxing@163.com <xjxliuxing@163.com>
2022-01-18 01:54:44 +0800

    kotlin ---> 1

---
+++ app/src/main/java/com/android/app/ui/activity/kotlin/ArrayAndListActivity.kt
@@ -0,0 +1,72 @@
+package com.android.app.ui.activity.kotlin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.android.app.databinding.ActivityArrayAndListBinding
+import com.android.helper.base.title.AppBaseBindingTitleActivity
+import com.android.helper.utils.LogUtil
+
+class ArrayAndListActivity : AppBaseBindingTitleActivity<ActivityArrayAndListBinding>() {
+
+    override fun setTitleContent(): String {
+        return "数组和集合"
+    }
+
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivityArrayAndListBinding {
+        return ActivityArrayAndListBinding.inflate(inflater, container, true)
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+        // 指定类型
+        var array1 = arrayOf(1, 2, 3)
+
+        // 指定长度
+        var array2 = BooleanArray(1)
+
+        // 不指定类型的数组，自动推断，但是不能为null
+        val array3 = arrayOf(5)
+
+        // array3[2] = null
+        // 可空的数组
+        val arrayOfNulls = arrayOfNulls<String>(3)
+        arrayOfNulls[1] = null
+        arrayOfNulls[2] = ""
+        val intArray = IntArray(4) { 21 }
+
+        // 角标便利
+        for (a in 0 until intArray.size) {
+            LogUtil.e("a 1:" + intArray[a])
+        }
+
+        // 高级角标便利
+        for (a in intArray.indices) {
+            LogUtil.e("a 2:" + intArray[a])
+        }
+
+        // 元素便利
+        for (element in intArray) {
+            LogUtil.e("a 3:$element")
+        }
+
+        // 同时便利角标和元素
+        for ((index, item) in intArray.withIndex()) {
+            LogUtil.e("角标：$index 元素：$item")
+        }
+
+        // forEach 循环,it代表数据的元素
+        intArray.forEach {
+            LogUtil.e("forEach:$it")
+        }
+
+        // 增强版的forEach
+        intArray.forEachIndexed { index, item ->
+            LogUtil.e("增强forEach:  index：$index   item:$item")
+        }
+
+        // -----------------------------------
+
+//        val arrayListOf = arrayListOf<String>()
+
+    }
+}
--- app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
+++ app/src/main/java/com/android/app/ui/activity/kotlin/KotlinMap.kt
@@ -2,10 +2,11 @@ package com.android.app.ui.activity.kotlin
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.android.app.R
 import com.android.app.databinding.ActivityKotlinMapBinding
 import com.android.helper.base.title.AppBaseBindingTitleActivity
-import com.android.helper.utils.LogUtil
 
 /**
  * @author XJX
@@ -14,56 +15,25 @@ import com.android.helper.utils.LogUtil
  */
 class KotlinMap : AppBaseBindingTitleActivity<ActivityKotlinMapBinding>() {
 
-    var ssss: String = "sss"
-
-    override fun initData(savedInstanceState: Bundle?) {
-        // 指定类型
-        var array1 = arrayOf(1, 2, 3)
-
-        // 指定长度
-        var array2 = BooleanArray(1)
-
-        // 不指定类型的数组，自动推断，但是不能为null
-        val array3 = arrayOf(5)
-
-        // array3[2] = null
-        // 可空的数组
-        val arrayOfNulls = arrayOfNulls<String>(3)
-        arrayOfNulls[1] = null
-        arrayOfNulls[2] = ""
-        val intArray = IntArray(4) { 21 }
-        // 角标便利
-        for (a in 0 until intArray.size) {
-            LogUtil.e("a 1:" + intArray[a])
-        }
-        // 高级角标便利
-        for (a in intArray.indices) {
-            LogUtil.e("a 2:" + intArray[a])
-
-        }
-        // 元素便利
-        for (element in intArray) {
-            LogUtil.e("a 3:$element")
-        }
-        // 同时便利角标和元素
-        for ((index, item) in intArray.withIndex()) {
-            LogUtil.e("角标：$index 元素：$item")
-        }
-        // forEach 循环,it代表数据的元素
-        intArray.forEach {
-            LogUtil.e("forEach:$it")
-        }
-        // 增强版的forEach
-        intArray.forEachIndexed { index, item ->
-            LogUtil.e("增强forEach:  index：$index   item:$item")
-        }
-    }
-
     override fun setTitleContent(): String {
-        return "ktolin集合"
+        return "kotlin集合"
     }
 
     override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivityKotlinMapBinding {
-        return ActivityKotlinMapBinding.inflate(inflater, container, false)
+        return ActivityKotlinMapBinding.inflate(inflater, container, true)
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+        setonClickListener(R.id.tv_array_and_list)
+    }
+
+    override fun onClick(v: View?) {
+        super.onClick(v)
+        when (v?.id) {
+            R.id.tv_array_and_list -> {
+                startActivity(ArrayAndListActivity::class.java)
+            }
+        }
     }
 }
