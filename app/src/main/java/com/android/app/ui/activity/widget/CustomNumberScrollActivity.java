4bca39e08484cc5b655d67da5b8aaec5287232e8
XJX <xjxliuxing@163.com>
2022-10-17 18:54:45 +0800

    测试滑动

--- app/src/main/java/com/android/app/ui/activity/widget/CustomNumberScrollActivity.java
+++ app/src/main/java/com/android/app/ui/activity/widget/CustomNumberScrollActivity.java
@@ -1,11 +1,13 @@
 package com.android.app.ui.activity.widget;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 import com.android.app.R;
+import com.android.app.widget.ScrollNumberView3;
 
 public class CustomNumberScrollActivity extends FragmentActivity {
 //
@@ -19,14 +21,19 @@ public class CustomNumberScrollActivity extends FragmentActivity {
 //        return "自定义数字滑动";
 //    }
 //
-//    @Override
-//    public void initData(Bundle savedInstanceState) {
-//
-//    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_number_scroll);
+
+        ScrollNumberView3 scrollNumberView3 = findViewById(R.id.sc3);
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollNumberView3.invalidate();
+            }
+        });
     }
 }
