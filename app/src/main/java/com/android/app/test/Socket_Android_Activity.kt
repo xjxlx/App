6b0aa204a0d86013452d769446f0c4c401f0df87
xjxliuxing <xjxliuxing@163.com>
2023-05-14 21:52:06 +0800

    android - socket - service

--- app/src/main/java/com/android/app/test/Socket_Android_Activity.kt
+++ app/src/main/java/com/android/app/test/Socket_Android_Activity.kt
@@ -2,17 +2,15 @@ package com.android.app.test
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import com.android.app.databinding.ActivitySocketAndroidBinding
+import com.android.app.R
 import com.android.apphelper2.utils.LogUtil
-import com.android.helper.base.BaseBindingActivity
+import com.android.helper.base.AppBaseActivity
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.net.ServerSocket
 import java.net.Socket
 
-class Socket_Android_Activity : BaseBindingActivity<ActivitySocketAndroidBinding>() {
+class Socket_Android_Activity : AppBaseActivity() {
 
     companion object {
         private val TAG = "Android端"
@@ -22,31 +20,30 @@ class Socket_Android_Activity : BaseBindingActivity<ActivitySocketAndroidBinding
         ServerThread().start()
     }
 
-    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): ActivitySocketAndroidBinding {
-        return ActivitySocketAndroidBinding.inflate(layoutInflater, container, true)
+    override fun getBaseLayout(): Int {
+        return R.layout.activity_socket_android
     }
 
     class ServerThread : Thread() {
-
         override fun run() {
             super.run()
-            var serverSocket: ServerSocket? = null;
-            try {
-                serverSocket = ServerSocket(19000);
-                while (true) {
-                    val socket = serverSocket.accept();
 
+            var serverSocket: ServerSocket? = null
+            try {
+                serverSocket = ServerSocket(19000)
+                while (true) {
+                    val socket = serverSocket.accept()
                     LogUtil.e(TAG, "接受连接")
-                    ClientThread(socket!!).start();
+                    ClientThread(socket!!).start()
                 }
             } catch (e: Exception) {
-                e.printStackTrace();
+                e.printStackTrace()
             } finally {
                 if (serverSocket != null) {
                     try {
-                        serverSocket.close();
+                        serverSocket.close()
                     } catch (e: Exception) {
-                        e.printStackTrace();
+                        e.printStackTrace()
                     }
                 }
             }
@@ -54,33 +51,32 @@ class Socket_Android_Activity : BaseBindingActivity<ActivitySocketAndroidBinding
     }
 
     private class ClientThread(val socket: Socket) : Thread() {
-
         init {
             LogUtil.e(TAG, "当前Socket:$socket")
         }
 
         override fun run() {
             super.run()
-            try {
-                val dis = DataInputStream(socket.getInputStream());
-                val dos = DataOutputStream(socket.getOutputStream());
 
+            try {
+                val dis = DataInputStream(socket.getInputStream())
+                val dos = DataOutputStream(socket.getOutputStream())
                 while (true) {
                     val data = dis.readUTF();
                     Log.d(TAG, "收到数据:$data");
 
                     //回写给客户端。
-                    val s = "手机时间:" + System.currentTimeMillis();
-                    dos.writeUTF(s);
-                    dos.flush();
+                    val s = "手机时间:" + System.currentTimeMillis()
+                    dos.writeUTF(s)
+                    dos.flush()
                 }
             } catch (e: Exception) {
-                e.printStackTrace();
+                e.printStackTrace()
             } finally {
                 try {
-                    socket.close();
+                    socket.close()
                 } catch (e: Exception) {
-                    e.printStackTrace();
+                    e.printStackTrace()
                 }
             }
         }
